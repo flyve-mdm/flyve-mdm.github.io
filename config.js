abeaeb78ec393bb80ef651d109cd238b8396d8ef
Gianfranco Manganiello <gmanganiello@teclib.com>
2017-09-13 19:01:35 -0400

    build(transfer): add files from flyve-mdm-web-dashboard

---
+++ config.js
@@ -0,0 +1,13 @@
+/**
+angular.module('FlyveMDM')
+.constant('GLPI_API_URL', 'https://demo.flyve.org/apirest.php')
+.constant('USER_TOKEN', 'an89HjkLbfdR')
+.constant('DEBUG', false)
+.constant('BUILD_ID', 0);
+ */
+
+angular.module('FlyveMDM')
+  .constant('GLPI_API_URL', 'https://dev.flyve.org/glpi/apirest.php')
+  .constant('USER_TOKEN', 'kbd2wdhj8hqgc6ggj20acdkdfb6gyxt4rc2hem9n')
+  .constant('DEBUG', false)
+  .constant('BUILD_ID', 0);
